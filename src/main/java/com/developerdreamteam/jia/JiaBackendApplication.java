9933d6c3c2494af96332519839843872821c57c3
Anurag <anuraghandique.dev@gmail.com>
2024-06-27 20:16:05 +0530

    Initial commit

---
+++ src/main/java/com/developerdreamteam/jia/JiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.developerdreamteam.jia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiaBackendApplication.class, args);
+    }
+
+}
